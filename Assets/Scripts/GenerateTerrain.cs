1e9756557e75bd45f4a17463780af2befab1f65a
XIEZHU2018 <905400324@qq.com>
2020-11-10 11:52:17 +0800

    提交

---
+++ Assets/Scripts/GenerateTerrain.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTerrain : MonoBehaviour
+{
+    List<GameObject> treeList = new List<GameObject>();
+
+    public GameObject[] fruitPrefabs;
+
+    void Start()
+    {
+        int heightScale = 10;
+        float detailScale = 20.0f;
+
+        Mesh mesh = this.GetComponent<MeshFilter>().mesh;
+        Vector3[] verts = mesh.vertices;
+        for (int i = 0; i < verts.Length; i++)
+        {
+            verts[i].y = Mathf.PerlinNoise((verts[i].x + this.transform.position.x) / detailScale,
+                                            (verts[i].z + this.transform.position.z) / detailScale) * heightScale;
+
+
+            if (verts[i].y > 7.5f// && Mathf.PerlinNoise((verts[i].x+5) / 10, (verts[i].z + 5) / 10) * 10 > 4.6
+            && Random.Range(0, 20) > 18)
+            {
+                GameObject newTree = ScrTreePoolCreate.getTree();
+
+                if (newTree != null)
+                {
+                    Vector3 treePos = new Vector3(
+                        verts[i].x + this.transform.position.x,
+                        verts[i].y - 1,
+                        verts[i].z + this.transform.position.z
+                    );
+                    newTree.transform.position = treePos;
+                    newTree.SetActive(true);
+                    treeList.Add(newTree);
+
+
+                    // create fruit
+                    if (Random.Range(0, 10) > 8)
+                    {
+                        // /*
+                        float plusX = Random.Range(5.0f, 10.0f);
+                        if (Random.Range(0, 10) >= 5)
+                        {
+                            plusX = -plusX;
+                        }
+                        float plusZ = Random.Range(5.0f, 10.0f);
+                        if (Random.Range(0, 10) >= 5)
+                        {
+                            plusZ = -plusZ;
+                        }
+
+                        Vector3 fruitPos = new Vector3(
+                            treePos.x + plusX,
+                            treePos.y + 10,
+                            treePos.z + plusZ
+                            );
+
+                        GameObject fruit = (GameObject)Instantiate(fruitPrefabs[Random.Range(0, fruitPrefabs.Length)], fruitPos, Quaternion.identity);
+                        fruit.transform.SetParent(this.transform);
+                        //*/
+                    }
+
+                }
+            }
+        }
+
+        mesh.vertices = verts;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        this.gameObject.AddComponent<MeshCollider>();
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnDestroy()
+    {
+        for (int i = 0; i < treeList.Count; i++)
+        {
+            if (treeList[i] != null)
+            {
+                treeList[i].SetActive(false);
+            }
+        }
+
+        treeList.Clear();
+    }
+}
---
+++ Assets/Scripts/ScrTreePoolCreate.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//大环境中静态物体的批量生成
+public class ScrTreePoolCreate : MonoBehaviour
+{
+    static int treeAmount = 600;
+    public GameObject[] treePrefabs;
+    static GameObject[] trees;
+
+    void Start()
+    {
+        trees = new GameObject[treeAmount];
+        int currentTreePrefab = 0;
+
+        for (int i = 0; i < treeAmount; i++) {
+            trees[i] = (GameObject) Instantiate(treePrefabs[currentTreePrefab], Vector3.zero, Quaternion.identity);
+            trees[i].transform.SetParent(this.transform);
+            trees[i].SetActive(false);
+
+
+            currentTreePrefab++;
+            if (currentTreePrefab >= treePrefabs.Length) {
+                currentTreePrefab = 0;
+            }
+        }
+    }
+
+    static public GameObject getTree() {
+
+        for (int i = 0; i < treeAmount; i++) {
+            if (!trees[i].activeSelf) {
+                return trees[i];
+            }
+        }
+        return null;
+    }
+}
